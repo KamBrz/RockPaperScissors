79f217ec8147e4477af793ab1b6d1a6b63adc8e5
Kamil <110668939+KamBrz@users.noreply.github.com>
2023-03-11 13:02:02 +0000

    Add a pseudocode to break down problems
    [Ticket: X]

---
+++ script.js
@@ -0,0 +1,5 @@
+//Rock-Paper-Scissors game
+//User inputs choice through a prompt
+//Computer inputs choice randomly based on array
+//Choices get compared over 5 games
+//Whoever gets a higher score wins
