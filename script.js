616ccc8c6f4e184c38a2cb9b2a51ba0bdaab7355
Kamil <110668939+KamBrz@users.noreply.github.com>
2023-03-11 14:16:56 +0000

    Add variables for the game, create game loop to 5
    Variables are made for adding up scores and total rounds.
    game function runs a 5-times loop adding scores to appropriate variables
    and returning a win/lose/draw at the end.
    
    [Ticket: X]

--- script.js
+++ script.js
@@ -5,19 +5,18 @@
 //Results get added up over 5 games
 //Final result displayed to the player
 
-//User inputs choice through a prompt
-let playerSelection = prompt("Choose: Rock, Paper, or Scissors?").toLowerCase();
+
 
 //Randomise computer selection based on array & return item from an array
 function getComputerChoice() {
     let arrChoice = ["rock","paper","scissors"];
     let randomise = Math.floor(Math.random()*arrChoice.length);
-    return returnItem = arrChoice[randomise];
+    return randomWord = arrChoice[randomise];
 }
 
 //Compare player and computers hands
 
-function compareHands () {
+function compareHands (playerSelection) {
     const computerSelection = getComputerChoice();
     if (playerSelection === computerSelection) {
         return ("It's a draw!");
@@ -31,3 +30,36 @@ function compareHands () {
         return (`You lose! ${computerSelection} beats ${playerSelection}!`)
     }
 }
+
+const totalRounds = 5;
+let playerScore = 0;
+let computerScore = 0;
+let drawScore = 0; 
+
+
+function game() {
+    for (let i = 0; i < totalRounds; i++) {
+        const playerSelection = prompt("Choose: Rock, Paper, or Scissors?").toLowerCase();
+        const roundResult = compareHands(playerSelection);
+        if (roundResult.startsWith ("You win")) {
+            playerScore++;
+        } else if (roundResult.startsWith("You lose")) {
+            computerScore++;
+        } else {
+            drawScore++;
+        }
+        console.log(`User score: ${playerScore}`);
+        console.log(`Computer score: ${computerScore}`);
+        console.log(`Draws: ${drawScore}`);
+    }
+
+    if (playerScore > computerScore) {
+        console.log(`You win ${playerScore}:${computerScore}`);
+    } else if (computerScore > playerScore) {
+        console.log(`You lose ${computerScore}:${playerScore}`)
+    } else {
+        console.log("It's a draw!")
+    }
+}
+
+game();
